5fee6a7847fcfd466f84719ca67ffa51d7938bdc
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-03-26 10:44:09 +0600

    Header and Product component done

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>CAR-EXCLUSIVE</h1>
+            <h2>Explore World Best Car !</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Component/Product/Product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+const Product = (props) => {
+    const {name,price,ratings,img}=props.product;
+    
+    return (
+        <div className='product'>
+            <img src={img} alt=''></img>
+            <div className='product-info'>
+                <h4 className='product-name'>{name}</h4>
+                <p className='product-price'>Price: ${price}</p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <div className='btn'>
+                <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'>
+                    <p className='btn-text'>Add to Cart</p>
+                    <FontAwesomeIcon icon={faShoppingCart }></FontAwesomeIcon>
+                </button>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Product;
